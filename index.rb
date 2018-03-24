5fc4e2870150e182667c4ff465fbd32700e0346a
menem salama <menem.slama@gmail.com>
2018-03-24 11:03:09 +0200

    parse all inputs and print parsed paths

--- index.rb
+++ index.rb
@@ -4,11 +4,6 @@ require 'pp'
 
 inputs = Dir.entries("inputs").select {|f| !File.directory? f}
 
-results = {
-  :sections => [],
-  :paragraphs => []
-}
-
 def get_tag_num(elm)
   elm[0][/\d/].to_i
 end
@@ -18,37 +13,51 @@ def is_h_tag(e)
   num.is_a?(Integer) and num != 0
 end
 
-tags = []
-page = Nokogiri::HTML(open("inputs/#{inputs[2]}"))
-page.css('h2, h3, h4, h5, h6, p').each do |link|
-  tags.push [link.name, link.text]
-end
+def parse(file_path)
+  results = {
+    :sections => [],
+    :paragraphs => []
+  }
 
-base_h_tag = nil
-
-tags.each do |e|
-  if base_h_tag == nil and is_h_tag(e)
-    base_h_tag = e
-  elsif !is_h_tag(e) and results[:sections].length == 0
-    results[:paragraphs].push(e[1])
-    next
+  tags = []
+  page = Nokogiri::HTML(open(file_path))
+  page.css('h2, h3, h4, h5, h6, p').each do |link|
+    tags.push [link.name, link.text]
   end
 
-  idx = results[:sections].length-1
-  if e[0] == base_h_tag[0]
-    results[:sections].push({ :section => e[1], :paragraphs => [] })
-  elsif !is_h_tag(e) and results[:sections][idx][:subsections].is_a?(Array)
-    sub_idx = results[:sections][idx][:subsections].length-1
-    results[:sections][idx][:subsections][sub_idx][:paragraphs].push e[1]
-  elsif is_h_tag(e)
-    results[:sections][idx][:subsections] = [] if results[:sections][idx][:subsections] == nil
-    results[:sections][idx][:subsections].push({ :subsection => e[1], :paragraphs => [] })
-  else
-    results[:sections][idx][:paragraphs].push e[1]
+  base_h_tag = nil
+
+  tags.each do |e|
+    if base_h_tag == nil and is_h_tag(e)
+      base_h_tag = e
+    elsif !is_h_tag(e) and results[:sections].length == 0
+      results[:paragraphs].push(e[1])
+      next
+    end
+
+    idx = results[:sections].length-1
+    if e[0] == base_h_tag[0]
+      results[:sections].push({ :section => e[1], :paragraphs => [] })
+    elsif !is_h_tag(e) and results[:sections][idx][:subsections].is_a?(Array)
+      sub_idx = results[:sections][idx][:subsections].length-1
+      results[:sections][idx][:subsections][sub_idx][:paragraphs].push e[1]
+    elsif is_h_tag(e)
+      results[:sections][idx][:subsections] = [] if results[:sections][idx][:subsections] == nil
+      results[:sections][idx][:subsections].push({ :subsection => e[1], :paragraphs => [] })
+    else
+      results[:sections][idx][:paragraphs].push e[1]
+    end
   end
 
+  results
 end
 
-File.open("testfile.json","w") do |f|
-  f.write(JSON.pretty_generate(results))
+inputs.each do |path|
+  results = parse "inputs/#{path}"
+  File.open("outputs/#{path}.json","w") do |f|
+    f.write(JSON.pretty_generate(results))
+  end
 end
+
+puts "Parsed inputs: "
+pp inputs
